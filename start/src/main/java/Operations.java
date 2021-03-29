f0bcf6f21ae1c98d33f3485a19a827a4c23cb2d4
Yaroslav <yaroslav.martynuik@gmail.com>
2021-03-30 00:04:57 +0300

    Expressions and operations

---
+++ start/src/main/java/Operations.java
@@ -0,0 +1,17 @@
+public class Operations {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println(2 + 2);
+        System.out.println(1/2);
+        System.out.println(1.0/2);
+        System.out.println(2 + "2");
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + 2 * 2);
+        System.out.println((2 + 2) * 2);
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+
+    }
+}
