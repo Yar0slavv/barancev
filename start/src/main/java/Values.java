ba567c44533b7ff29ea506e4181f32c35db5a675
Yaroslav <yaroslav.martynuik@gmail.com>
2021-03-30 22:20:18 +0300

    Variable and value types

--- start/src/main/java/Values.java
+++ start/src/main/java/Values.java
@@ -2,7 +2,18 @@ public class Values {
     public static void main(String[] args) {
         System.out.println("Hello!" + "World!");
         int s = 6;
-        int l = s * s;
+        int l = s + s;
         System.out.println("Площадь квадрата со стороной " + s + " = " + l);
+
+        double k = 8.1;
+        boolean q = true;
+        String name = "Oleg";
+
+        System.out.print(k);
+        System.out.println(" "+q);
+        System.out.print(name);
+
+
+
     }
 }
