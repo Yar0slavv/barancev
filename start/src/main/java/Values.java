ce7865990549de3cf14b6bfe474a7783428e4e81
Yaroslav <yaroslav.martynuik@gmail.com>
2021-03-30 22:07:51 +0300

    Variables and Values

---
+++ start/src/main/java/Values.java
@@ -0,0 +1,8 @@
+public class Values {
+    public static void main(String[] args) {
+        System.out.println("Hello!" + "World!");
+        int s = 6;
+        int l = s * s;
+        System.out.println("Площадь квадрата со стороной " + s + " = " + l);
+    }
+}
